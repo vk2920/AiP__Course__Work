b70ab3122f4a47e9b3d8433cfa9529bba8432507
vk292 <vk292@THINKBOOK-9160>
2022-09-16 16:50:53 +0300

    Добавьте файлы проекта.

---
+++ menu.h
@@ -0,0 +1,5 @@
+#pragma once
+int* get_size();
+int test_input();
+int select_from_menu(const char** _menu, const int _mic, int _exit_code);
+int menu_new(const char** _menu_item_list, const int _menu_items_count, const int _exit_answer);
