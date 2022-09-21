a47ed78526dce01b48ee1291ee44c125153b1fe5
Вадим Контарев <vk2920@yandex.ru>
2022-09-21 17:04:08 +0300

    Реализованы функции для работы со списком, но прога сломана

--- AiP_Course_Work.c
+++ AiP_Course_Work.c
@@ -6,8 +6,84 @@
 #include <conio.h>
 #include "menu.h"
 
-int main()
-{
+#ifndef ushort
+#define ushort unsigned short
+#endif
+#ifndef uint
+#define uint unsigned int
+#endif
+
+typedef struct student {
+    char group[7];           // Шифр группы (6 символов)
+    char surname[16];        // Фамилия студента (15 символов)
+    ushort birth_year;       // Год рождения студента (0 - 65535)
+    byte woman;              // Пол студента (0 или 1)
+    ushort skipped_hours;    // Пропущенные часы
+    ushort acquitted_skips;  // Оправданные часы
+} student;
+
+typedef struct list_item {
+    student data;
+    list_item* prev;
+    list_item* next;
+} list_item;
+
+typedef struct list_header {
+    list_item* first;
+    list_item* last;
+    uint len;
+} list_header;
+
+/// <summary>
+/// Функция для создания пустого списка.
+/// </summary>
+/// <returns>Указатель на заголовок созданного списка</returns>
+list_header* create_empty_list() {
+    list_header* res = (list_header*)calloc(1, sizeof(list_header));
+    res->first = NULL;
+    res->last = NULL;
+    res->len = 0;
+    return res;
+}
+
+/// <summary>
+/// Функция для удаления списка, включая все его элементы
+/// </summary>
+/// <param name="_lh">Указатель на заголовок удаляемого списка</param>
+void delete_list(list_header* _lh) {
+    if (!_lh) return;
+    list_item* first = _lh->first;
+    while (first) {
+        list_item* second = first->next;
+        free(first);
+        first = second;
+        first->prev = NULL;
+    }
+    free(_lh);
+}
+
+int add_new_item_to_list(list_header* _lh, student _stud) {
+    if (!_lh) return 0;
+    list_item* first = _lh->first;
+    while (first) first = first->next;
+    first->next = (list_item*)calloc(1, sizeof(list_item));
+    if (!(first->next)) {
+        first->next = NULL;
+        return 0;
+    }
+    first->next->data = _stud;
+    first->next->prev = first;
+    first->next->next = NULL;
+    _lh->last = first->next;
+    return 1;
+}
+
+student create_student_from_data(char group[7], char surname[16], ushort birth_year, byte woman, ushort skipped_hours, ushort acquitted_skips) {
+    student res = { group, surname, birth_year, woman, skipped_hours, acquitted_skips };
+    return res;
+}
+
+int main() {
     SetConsoleOutputCP(1251);
     const char* menu[menu_items_count] = {
         "Создать очередь",
@@ -30,9 +106,13 @@ int main()
         "Удалить второй класс",
         "Выйти из программы"
     };
-    // select_from_menu(menu, menu_items_count, menu_items_count - 1);
-    // test_input();
-    _getch();
-    enter_data(menu, menu_items_count, NULL);
+    list_header* header = NULL;
+    for (uint _i = 0; _i < 4294967295; _i++) {
+        header = create_empty_list();
+        for (uint _j = 0; _j < rand()%999 + 1; _j++)
+            add_new_item_to_list(header, create_student_from_data("IS21-3", "Kontarev VadimV", rand()%65535, rand()%2, rand()%65535, rand()%65535));
+
+        delete_list(header);
+    }
     return 0;
 }
